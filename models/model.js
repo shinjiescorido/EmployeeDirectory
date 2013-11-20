eca4f7c75d74a634ad47be452bccd60dbb54a412
shinji escorido <shinji.escorido@gmail.com>
2013-11-20 16:30:11 +0800

    Defined whole scheme for the Employee Directory Project
    customFields
    label (String)
    fieldType (Int)
    accessModifier (Bool)
    values[] (String of Array)
    required (Bool)
    
    
    User
    email (String) 
    password (String)
    firstName (String)
    lastName (String)
    jobPosition (String)
    photo (String)
    isActive (Bool)
    field (customFields collection)
    
    all basic informations are required except photo and custom fields.
    
    
    Notification
    message (String)
    recipient (_id)
    sender (_id)
    shown (Bool)
    isBroadcast (Bool)

--- models/model.js
+++ models/model.js
@@ -1,6 +1,13 @@
 module.exports = function (app) {
 
-mongoose.connect('mongodb://root:root@ds039507.mongolab.com:39507/empdir', function (err) {
+   var mongoose = require('mongoose');
+
+/** 
+* MONGOOSE DB CONNECT
+* -------------------------------------------------------------------------------------------------
+* used localhost remote db for testing purposes
+**/
+mongoose.connect("mongodb://localhost/loggers", function (err) {
   if (err) {
       console.log(err);
   } else {
@@ -8,11 +15,49 @@ mongoose.connect('mongodb://root:root@ds039507.mongolab.com:39507/empdir', funct
   }
 });
 
-var Usr = mongoose.model('Users', mongoose.Schema({
-    username: String,
-    password: String,
-    created: {type: Date, default: Date.now}
+	//Create reference for data type objectId
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+// customfields schema 
+var customFields = mongoose.model('customFields',mongoose.Schema({
+			
+			label: { type: String, required: true},
+			fieldType: { type: Number, required: true},
+			accessModifier: Boolean,
+			values:[String],
+			required: Boolean
+
+}));
+
+// usr schema
+var User = mongoose.model('Users', mongoose.Schema({
+			email: { type: String, required: true,validate: [validateEmail, 'Invalid email']}, 
+			password: { type: String, required: true},
+			firstName: { type: String, required: true},
+			lastName: { type: String, required: true},
+			jobPosition: { type: String, required: true},
+			photo: String,
+			isActive: Boolean,
+			field: [customFields]
   })
 );
 
+// notifications schema
+var notification = mongoose.model('notification', mongoose.Schema({
+			message: String,
+			recipient: ObjectId,
+			sender: ObjectId,
+			shown :Boolean,
+			isBroadcast: Boolean
+}));
+
+//Email Validation method
+function validateEmail(v){
+
+	var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+   return emailRegex.test(v); 
+
 }
+
+}// end exports
